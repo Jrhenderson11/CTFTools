984cca868a8ee37de061622662a86b9eb8a07d51
James <jrhenderson11@gmail.com>
2018-04-05 23:19:55 +0100

    added colour coding to caesar tool for correctness-analysis

--- Crypto/caesar.py
+++ Crypto/caesar.py
@@ -1,11 +1,8 @@
-
-
 def char_to_int(char):
 	return ord(char)-96
 
 def int_to_char(num):
 	return chr((num%26)+96)
-
 
 text = "apple"
 res = []
@@ -16,4 +13,22 @@ for i in range(1, 27):
 		shifted = int_to_char(char_to_int(char) + i)
 		current += shifted
 	res.append(current)
-	print(str(i) + ": " + current)
+
+
+file = open("/usr/share/dict/british-english")
+words = file.read().split("\n")
+file.close()
+i=0
+for result in res:
+	i += 1
+	current = ""
+	for word in result.split(" "):
+		if word in words:
+			current += '\033[92m\033[1m' + word + '\033[0m'
+		else:
+			current += word
+		current += " "
+		print(str(i) + ": " + current)
+#TODO:
+# add command line params
+#improve dict with pickle / hash table
