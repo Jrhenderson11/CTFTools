2677f770abcece54e43b56875eefd9410ce4e6c1
James <jrhenderson11@gmail.com>
2019-01-31 18:39:49 +0000

    Updated frequency analysis

--- crypto/crypter.py
+++ crypto/crypter.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 #import pycrypto
 from Crypto.Cipher import AES
 
@@ -8,4 +10,4 @@ ciphertext = "e7784e590218b8899c2af32aabd29e7fa4cf81391198dd0df74debefae853123"
 obj2 = AES.new(key.decode("hex"), AES.MODE_CBC, IV.decode("hex"))
 
 #ciphertext = obj.encrypt(message)
-print obj2.decrypt(ciphertext.decode("hex"))
+print(obj2.decrypt(ciphertext.decode("hex")))
--- crypto/freqanalysis.py
+++ crypto/freqanalysis.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-
 import re
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-#return set of all repeated text strings
-def get_repetitions(base, text):
+# Return set of all repeated text strings
+def get_repetitions(text, base=""):
 	chars = set(text)
 	repetitions = set()
 	for char in chars:
 		if (len(re.findall(base+char, text))>1):
-			repetitions.update({base+char} | get_repetitions(base+char, text))
+			repetitions.update({base+char} | get_repetitions(text, base=base+char))
 	return repetitions
 
-#get repetitions without intersecting subsets (e.g "abcdabc" returns "abc" not "ab" "bc" "abc")
+# Get repetitions without intersecting subsets (e.g "abcdabc" returns "abc" not "ab" "bc" "abc")
 def get_occluding_repetitions(base, text):
-	repetitions = (get_repetitions(base,text))
+	repetitions = (get_repetitions(text))
 	return filter(lambda x: x not in " ".join(repetitions - {x}), repetitions)
-
 
 def display_freq_chart(text):
 	freqs = dict()
@@ -40,10 +38,10 @@ def display_freq_chart(text):
 	plt.show()
 
 if __name__ == '__main__':
-	text = "efgggaaaabbbccd"
+	text = "abcdabc"
 	if len(sys.argv) >1:
 		text = sys.argv[1]
-	print("chars: " + str(list(filter(lambda x: len(x)!=1, get_repetitions("", text)))))
+	print("repeating groups: " + str(list(filter(lambda x: len(x)!=1, get_occluding_repetitions("", text)))))
 	display_freq_chart(text)
 
 #TODO:
